9c64fbda7116e7e31f19a22285b7f03127f99917
WCX1024979076 <1024979076@qq.com>
2021-02-11 19:53:42 +0800

    上传

---
+++ nonebot/awesome_bot/plugins/ai/config.py
@@ -0,0 +1,8 @@
+# Author: BeiYu
+# Github: https://github.com/beiyuouo
+# Date  : 2021/1/11 22:42
+# Description:
+
+BAIDU_APPID = '23583154'
+BAIDU_API_KEY = 'siCGILVMdCOu0SpDzpEAPKkV'
+BAIDU_SECRET_KEY = 'VbNnOnH2K94ujCcuba7lDfs2zLtm5XQx'
